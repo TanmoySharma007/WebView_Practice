212777d84a383ffc95f8cd3cc861229113f1868d
Tanmoy Sharma <tonmoytonu55@gmail.com>
2022-09-13 12:15:24 +0600

    Web view Practise

---
+++ app/src/main/java/com/example/webview_practice/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.webview_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        webView =  findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://yomovies.cloud/");
+
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+                progressBar.setVisibility(View.VISIBLE);
+
+
+
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                progressBar.setVisibility(View.GONE);
+
+
+            }
+
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+
+                progressBar.setProgress(newProgress);
+
+
+
+            }
+
+        });
+
+
+    }
+}
